c285ac9689001ff6d9d54b83a94598845267bf54
Meghna <msrivastava@voltus.co>
2025-03-29 12:40:20 -0700

    add memories

--- src/pages/Memories.js
+++ src/pages/Memories.js
@@ -38,6 +38,36 @@ const Memories = () => {
           <br></br>
           <p>- Manish</p>
         </div>
+        <div class="memory">
+          <p>
+            In the nine years he lived with us in Cary, Finn brought endless joy
+            and warmth to our home. I loved his gentle, sweet nature and how he
+            would plop in front of me for pets and belly rubs. I have such vivid
+            memories of his soft fur and bright blue eyes. His playful spirit
+            kept us entertained, as he explored every room (even the forbidden
+            ones), window sills, and the screened porch. Finn's memory remains
+            deeply cherished in my heart. I will forever miss his tender purrs,
+            soft snuggles, and the endless curiosity that made him so special.
+            Rest in peace, sweet Finn. You were a gift to our lives.
+          </p>
+          <br></br>
+          <p>- Alka</p>
+        </div>
+        <div class="memory">
+          <p>
+            Finn was the most beautiful cat I had ever lain eyes on and his
+            personality fully matched. With the sweetness of a candy cane and
+            the coat of vanilla ice cream, his cuddles would never fail.
+            Whenever I got the pleasure of pet sitting, the first course of
+            action would be to immediately say hello - and his hugs were always
+            divine. I would listen to his pitter patters as he would follow me
+            to the food bowl, with purrs filling the corridor. Though he was not
+            always keen to stay close by, he would always ensure to say goodbye!
+            Love you so much Finn, and will always miss your presence.
+          </p>
+          <br></br>
+          <p>- Hannah, channeling 13yo Hannah</p>
+        </div>
       </div>
     </div>
   );
