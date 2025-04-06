5026c6056b66602a8caee796a1fc19b01fa60e9e
Meghna <msrivastava@voltus.co>
2025-04-06 14:49:16 -0700

    add memories and update formatting

--- src/pages/Memories.js
+++ src/pages/Memories.js
@@ -1,4 +1,6 @@
 import "../styles.css";
+import with_buaji from "../images/with_buaji_cropped.jpg";
+import cat_candle from "../images/cat-candle-512x512.png";
 import React from "react";
 
 const Memories = () => {
@@ -13,7 +15,7 @@ const Memories = () => {
       </p>
       <br></br>
       <div class="memory-gallery">
-        <div class="memory">
+        <div class="memory-red">
           <p>
             Something that got left out of the main "Finn Story" was the
             all-important Finn the Movie! A very poorly planned and executed
@@ -25,8 +27,12 @@ const Memories = () => {
           </p>
           <br></br>
           <p>- Meghna</p>
+          <br></br>
+          <br></br>
+          <br></br>
+          <img className="mini-img" src={cat_candle} />
         </div>
-        <div class="memory">
+        <div class="memory-yellow">
           <p>
             I have never been much of a pet person. After Alka and I finally
             agreed to Meghna's repeated requests to get a cat in Jan 2011, I
@@ -37,8 +43,12 @@ const Memories = () => {
           </p>
           <br></br>
           <p>- Manish</p>
+          <br></br>
+          <br></br>
+          <br></br>
+          <img className="mini-img" src={cat_candle} />
         </div>
-        <div class="memory">
+        <div class="memory-green">
           <p>
             In the nine years he lived with us in Cary, Finn brought endless joy
             and warmth to our home. I loved his gentle, sweet nature and how he
@@ -52,8 +62,10 @@ const Memories = () => {
           </p>
           <br></br>
           <p>- Alka</p>
+          <br></br>
+          <img className="mini-img" src={cat_candle} />
         </div>
-        <div class="memory">
+        <div class="memory-blue">
           <p>
             Finn was the most beautiful cat I had ever lain eyes on and his
             personality fully matched. With the sweetness of a candy cane and
@@ -67,6 +79,160 @@ const Memories = () => {
           </p>
           <br></br>
           <p>- Hannah, channeling 13yo Hannah</p>
+          <br></br>
+          <img className="mini-img" src={cat_candle} />
+        </div>
+        <div class="memory-purple">
+          <p>
+            I have countless memories of the time I spent with Finn, but one
+            that stands out is the first time we tried to feed him wet food. It
+            seemed like Finn struggled to identify it as food and instead just
+            licked it like the silly baby he was. He did eventually learn to
+            devour the wet food with the same gusto as he did his dry food. Finn
+            was such a sweet and lovely cat, and I will always love and miss
+            him.
+          </p>
+          <br></br>
+          <p>- Rishie</p>
+          <br></br>
+          <br></br>
+          <img className="mini-img" src={cat_candle} />
+        </div>
+        <div class="memory-red">
+          <p>
+            I wanted to take the chance to share my memory of Finn. He was
+            absolutely the most cuddly furry cat I have ever seen. I remember
+            him sitting on the carpet like a majestic lion, and then suddenly
+            rolling on his back to be petted. I'm gonna share a picture of Finn
+            with my mom which I think is beautiful. She too remembers him very
+            fondly.
+          </p>
+          <br></br>
+          <p>- Nilu Bua</p>
+          <br></br>
+          <img className="small-img" src={with_buaji} />
+        </div>
+        <div class="memory-yellow">
+          <p>
+            I remember Finn when we used to visit your old house in Highcroft
+            Village. He was handsome and very cute too, always so well behaved.
+            I know he was very loving and loyal especially to you. Even though
+            Finn is no longer with us, the love and joy he gave you will stay
+            with you forever.
+          </p>
+          <br></br>
+          <p>- Nital Auntie</p>
+          <br></br>
+          <br></br>
+          <img className="mini-img" src={cat_candle} />
+        </div>
+        <div class="memory-green">
+          <p>
+            I have so many beautiful memories of dinner get togethers at your
+            home. Finn was always affectionate (although all the kids running
+            around must have been so overwhelming). Your love for him was so
+            pure and visible in the way you held him. I can't even imagine the
+            pain of not having him around. Mia and all of us send hugs and love
+            to you.
+          </p>
+          <br></br>
+          <p>- Seema Auntie</p>
+          <br></br>
+          <br></br>
+          <img className="mini-img" src={cat_candle} />
+        </div>
+        <div class="memory-blue">
+          <p>
+            As one year anniversary of Finn's passing is approaching we all are
+            remembering him in a special way. What a lovely life of Finn! Like
+            your dad, I'm not much of a pet person though after reading Finn's
+            story and your love for him I felt a jolt of joy. It's extremely sad
+            to loose someone close to you, spending 13.5 years together and
+            above all loved one like you loved Finn - your only love at first
+            sight! At the same time inevitable does happen and that's the
+            almighty's way which we all have to accept and make the best out of
+            it, which you have done so by sharing such a lovely story of you and
+            Finn! I'm confident that he is still showing you with love in its
+            absolute purest form. Love you both!
+          </p>
+          <br></br>
+          <p>- Bimal Uncle</p>
+          <br></br>
+          <img className="mini-img" src={cat_candle} />
+        </div>
+        <div class="memory-purple-large">
+          <p>Today marks one year without my sweet Finn.</p>
+          <br></br>
+          <p>
+            When I was in college, I used to count down the days until the next
+            visit home as "Finn-less days." On the day of my travel back I would
+            rejoice that no Finn-less days remained. But now, unfortunately, all
+            I have are Finn-less days. It's crazy to think that 365 of them have
+            passed. I still think about Finn every day, and I miss him. I miss
+            his trotsky-esque footsteps, his voluminous fur, his many meows
+            (from sweet to indignant to almost whimpering). I miss his giant
+            head. His fixation with the shower in the Davis condo. His DEEP
+            sniffing of a bush outside the Madison Valley house where many
+            passing dogs would pee. I miss Finn burrowing season. The way he
+            would step on my hair and meow until I let him under the blanket.
+            The satisfied purr that would start up immediately once he was
+            safely snuggled against my side. I miss the little droplets of water
+            that he'd get on his nose after drinking from the fountain. I just
+            miss my little guy. I'm comforted knowing that he was spoiled
+            absolutely rotten for his entire life, showered with love not just
+            from me but from many others. Thank you Finn, for all of the
+            beautiful memories you've left us with. I feel lucky to have so much
+            to miss.
+          </p>
+          <br></br>
+          <p>- Meghna</p>
+          <br></br>
+          <img className="mini-img" src={cat_candle} />
+        </div>
+        <div class="memory-red-large">
+          <p>
+            It took some time for Finn to warm up to me, after we first met. And
+            I won't say that I never took offense (he would swat at my head, my
+            hair, my shoulders, when I walked past him on his cat tree,
+            something that happened so frequently they were coined "drive-bys"),
+            but I did understand it. I was bigger. I was stronger. Some may say
+            more beautiful. But most of all, I was superior to Finn in one
+            crucial way that I'm sure he held a grudge over: I am much, much
+            better at being comfortable.{" "}
+          </p>
+          <br></br>
+          <p>
+            Finn had a talent for it himself, but he was raw, unformed. He
+            figured out how to burrow to keep himself warm in the coldest
+            months, worming his way solo under the covers of a made bed by
+            himself for Megh to find him as a lump at the end of the day. But he
+            had no idea what a blanket cave was, or that he could even tap into
+            something incredible by pairing a burrow with a warm body.The
+            turning point in our relationship was the first Christmas we spent
+            together, alone, with Meghna gone and galivanting on the East Coast.
+            That's when we formed a truce that blossomed into a beautiful bond.
+            Through persistence, hard work, and a Megh-sized hole in his heart,
+            I was able to coax him under the blanket with me in bed. First, he'd
+            stay for seconds. Then minutes. Then, for hours at a time.
+            Eventually, I'd have one hand on my phone, or propping up a book,
+            and one hand on Finn, his body temperature soaring and his chest
+            crackling with ambient pleasure. Drive-bys became a thing of the
+            past.{" "}
+          </p>
+          <br></br>
+          <p>
+            It would've never worked if Meghna were there. He was desperate,
+            pathetic, and pliable with her gone. Together, we built a Christmas
+            miracle: Finn was even more annoying than before. From that time on,
+            Finn came to realize that the best burrowing is burrowing done with
+            the people you love, and that if you simply scream in their ears at
+            six in the morning, you don't even need to burrow yourself. They'll
+            sleepily lift the blanket up for you, and let you right inside.
+          </p>
+          <br></br>
+          <p>Love you, buddy.</p>
+          <br></br>
+          <p>- Nina</p>
         </div>
       </div>
     </div>
